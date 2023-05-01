d7fff50bd49a45644eab6c905a13a487c66cfae5
shrvn12 <singhshravan1208@gmail.com>
2023-05-01 14:44:43 +0530

    added google o auth

--- configs/google.oauth.js
+++ configs/google.oauth.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://thetechproject.cyclic.app/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
       return cb(null, profile);
